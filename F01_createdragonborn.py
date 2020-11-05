23b2dd5f00c76cb00957df44fa45d62d2e210a86
alizayn10 <alizayn.murteza@gmail.com>
2020-11-05 23:34:09 +0700

    f01

---
+++ F01_createdragonborn.py
@@ -0,0 +1,26 @@
+import csv
+def createdragonborn(dragonborn_data):
+    print ("$ create newChar")
+    nama = input("Nama Dragonborn: ")
+    i=0
+    print("newChar created")
+    print("Welcome "+str(nama)+" to the world of Skuyrim!"
+    print("Choose one faction: ")
+    print("1. Empire (+ 1 atatck)")
+    print("2. Stormcloaks (+ 1 magic)")
+    faction = int(input("Masukkan nomor faction diatas(1/2: "))
+    while True:
+        if faction == 1:
+            newchar = [str(i), str(nama), '100', '100', '1', '6', '5', '5', '0', 'null']
+            dragonborn_data += [newchar]
+            currentuser = newchar
+            break
+        elif faction == 2:
+            newchar = [str(i), str(nama), '100', '100', '1', '5', '6', '5', '0', 'null']
+            dragonborn_data += [newchar]
+            currentuser = newchar
+            break
+        else :
+            print("Nomor yang anda masukkan tidak sesuai!")
+            faction = int(input("Masukkan nomor faction diatas (1/2): "))
+    return currentuser
