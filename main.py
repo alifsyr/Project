c329018c8661c3179c79d0ee29c0febb2fb1b8a3
alifsyr <alifsyr@gmail.com>
2020-11-05 13:36:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Tugas Besar IF1210 Dasar pemrograman
 # Kelompok Stormcloak
+import F11_savegameandloadgame 
+
 
 '''
 Zachrandika Alif Syahreza
@@ -7,11 +9,15 @@ Zachrandika Alif Syahreza
 
 # KAMUS GLOBAL
 '''
-
+    dragonborn_data : array of array of string
+    item_data       : array of array of string
+    monster_data    : array of array of string
+    sidequest_data  : array of array of string
 '''
 
 endprogram      = False
 
 while (not endprogram):
-    
+    print("Welcome to Skuyrim")
+    dragonbron_data, item_data, monster_data, sidequest_data = F11_savegmeandloadgame.load()
     command = str(input("$ "))
