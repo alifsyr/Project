3bcc90714e404de7da676682d40aeefe3fec721b
alifsyr <alifsyr@gmail.com>
2020-11-06 00:07:31 +0700

    update main & F04

--- main.py
+++ main.py
@@ -37,23 +37,3 @@ while (not endprogram):
             F11_savegameandloadgame.save(data, names)
         endprogram = True
 
-# Untuk membuat object(Final boss= "Alduskuy"), digunakan fungsi class. di F08 saya sudah membuat final_bossnya dengan ketentuan tertentu
-class final_boss:
-    def __init__(self, Attack, Defense, BOSS_HP):
-        self.Attlow = -30
-        self.Atthigh = +70
-        self.MaxHP = BOSS_HP
-        self.Def = Defense
-        self.HP = BOSS_HP
-
-    def gen_dmg(self):
-        return random.randrange(self.Attlow, self.Atthigh)
-
-    def hp_boss(self):
-        return self.HP
-
-    def dmg_taken(self, dmg):
-        self.HP = self.HP - dmg
-        if ((self.HP) <= 0):
-            self.HP = 0
-        return self.HP
