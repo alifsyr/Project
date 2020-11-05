788cdfe8f94785aa4e9698b81402734195a5a1eb
alifsyr <alifsyr@gmail.com>
2020-11-05 14:45:11 +0700

    Update F11_ savegameandloadgame.py

--- F11_ savegameandloadgame.py
+++ F11_ savegameandloadgame.py
@@ -1,2 +1,16 @@
 import csv
 
+def load():
+    dragonborn_data        = loadfile("data/Dragonborn.csv")
+    item_data              = loadfile("data/Item.csv")
+    monster_data           = loadfile("data/Monster.csv")
+    sidequest_data         = loadfile("data/Sidequest.csv")
+    
+    return dragonborn_data, item_data, monster_data, sidequest_data
+
+def loadfile(x):
+    with open(x) as csvfile:
+        reader = csv.reader (csvfile,delimiter=',')
+        data   = [row for row in reader]
+    
+    return data
