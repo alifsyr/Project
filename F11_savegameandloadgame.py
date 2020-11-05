63f47e74e49f095b7648ebb9bfbd06ef086f810d
alifsyr <alifsyr@gmail.com>
2020-11-05 22:25:59 +0700

    Update F11_savegameandloadgame.py

--- F11_savegameandloadgame.py
+++ F11_savegameandloadgame.py
@@ -14,3 +14,18 @@ def loadfile(x):
         data   = [row for row in reader]
     
     return data
+
+def save(data, names):
+    import modules
+    folderDirectory = "data/"
+    for i in range(modules.panjang(names)):     # Melakukan looping berdasarkan panjang array names
+        name = str(folderDirectory + names)     # input nama file csv
+        writeFile(name, data[i])    # Memanggil fungsi writeFile
+    print('Data berhasil disimpan!')
+    
+def writeFile(namaFile, arrayData):
+    
+    with open(namaFile, mode='w',newline='') as csvfile:      # Membuka file dari hasil input name pada fungsi save dengan fungsi open dan menggunakan metode w untuk menulis 
+        writer = csv.writer(csvfile)
+        for row in arrayData:
+            writer.writerow(row)
