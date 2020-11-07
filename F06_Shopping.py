62cd7b5398a0132e0acf72737ec8e77866e77225
dimasanshari <dimasfarhan123@gmail.com>
2020-11-08 01:17:31 +0700

    update shopping.py

--- F06_Shopping.py
+++ F06_Shopping.py
@@ -13,9 +13,26 @@ def shop(currentUser, item_data):
                         print(str(num), i[1], "+" + i[3], "mgc", "(" + i[8], "gold)")
                     elif (int(i[2]) == 0 & int(i[3] == 0)):
                         print(str(num), i[1], "+" + i[4], "def", "(" + i[8], "gold)")
+                    else:
+                        print("99.exit")
                     num += 1
         print("You have 0 gold")
-        print("buy: ")
+        inp = int(input("buy: "))
+        x = 1
+        for i in item_data:
+            if(i[0] != "ID"):
+                if (int(i[7]==2)):
+                    if (inp == 1):
+                        print("item", i[1], "has been bought add", i[2], "attack to player")
+                        break
+                    elif (inp == 2):
+                        print("item", i[1], "has been bought add", i[3], "magic to player")
+                        break
+                    elif (inp == 3):
+                        print("item", i[1], "has been bought add", i[4], "defense to player")
+                        break
+                    else:
+                        exit()
 
     elif ((currentUser[10]) == "Solitude"):
         num = 1
@@ -26,6 +43,19 @@ def shop(currentUser, item_data):
                         print(str(num), i[1], "+" + i[5], "luck", "("+i[8], "gold)")
                     elif (int(i[5]) == 0):
                         print(str(num), i[1], "+" + i[6], "HP", "("+i[8], "gold)")
+                    else:
+                        print("99.exit")
                     num += 1
         print("You have 0 gold")
-        print("buy: ")
+        inp = int(input("buy: "))
+        j = 1
+        for i  in item_data:
+            if(i[0] != "ID"):
+                if (inp ==1):
+                    print("item", i[1], "has been bought add", i[6], "HP to player")
+                    break
+                elif (inp == 2):
+                    print("item", i[1], "has been bought add", i[5], "Luck to player")
+                    break
+                else:
+                    exit()
