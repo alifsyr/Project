2e099f26bb2298f390b367564cf4f888d6e25048
alifsyr <alifsyr@gmail.com>
2020-11-08 01:29:35 +0700

    update F01 and F11

--- F01_createdragonborn.py
+++ F01_createdragonborn.py
@@ -1,26 +1,19 @@
 def createdragonborn(dragonborn_data,currentUser):
-    nama = str(input("Nama Dragonborn: ")).capitalize().split()
-    for i in (dragonborn_data):
-        if i[0] != "ID":
-            maks = 0
-            if int(i[0]) > maks:
-                maks = int(i[0])
-    ID = maks + 1
-
-    print("newChar created")
-    print("Welcome "+str(nama)+" to the world of Skuyrim!")
+    nama = str(input("Masukan nama character: ")).capitalize()
+    print(nama,"created")
+    print("Welcome "+nama+" to the world of Skuyrim!")
     print("Choose one faction: ")
     print("1. Empire (+ 1 attack)")
     print("2. Stormcloaks (+ 1 magic)")
     faction = int(input("Masukkan nomor faction diatas(1/2): "))
     while True:
         if faction == 1:
-            newchar = [str(ID),str(nama),'100','100','1','6','5','5','0','0','null']
+            newchar = [str(generateid(dragonborn_data)),nama,'100','100','1','6','5','5','0','0','null']
             dragonborn_data += [newchar]
             currentUser = newchar
             break
         elif faction == 2:
-            newchar = [str(ID),str(nama),'100','100','1','5','6','5','0','0','null']
+            newchar = [str(generateid(dragonborn_data)),nama,'100','100','1','5','6','5','0','0','null']
             dragonborn_data += [newchar]
             currentUser = newchar
             break
@@ -29,3 +22,12 @@ def createdragonborn(dragonborn_data,currentUser):
             faction = int(input("Masukkan nomor faction diatas (1/2): "))
 
     return dragonborn_data, currentUser
+
+def generateid(dragonborn_data):
+    for i in (dragonborn_data):
+        if i[0] != "ID":
+            maks = 0
+            if int(i[0]) > maks:
+                maks = int(i[0])
+    ID = maks + 1
+    return ID
