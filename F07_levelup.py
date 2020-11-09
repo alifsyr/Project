43456cabfb1370e282465ad1b510a8dea035fedd
erwinfattah <68496276+erwinfattah@users.noreply.github.com>
2020-11-09 20:28:33 +0700

    nambah F05 dan F09

---
+++ F07_levelup.py
@@ -0,0 +1,35 @@
+def Level_Up():
+     print("You got "+str(exp)+" EXP")
+     print("Choose what attribute to increase :")
+     print("1.Health\n2.Attack\n3.Defense\n4.Magic\n5.Luck")
+     add=int(input("add attribute : "))
+     if add==1 or add==2 or add==3 or add=4 or add==5 :
+          if add==1 :
+                print("Your max Health has increased to "+str(exp+...)+" health")
+                print("Your current health also increased also to "+str(exp+Health)+" health")
+          elif add==2 :
+                print("Your max Attack has increased to "+str(exp+...)+" attack")
+                print("Your current Attack also increased also to "+str(exp+Attack)+" attack")
+          elif add==3 :
+                print("Your max Defense has increased to "+str(exp+...)+" Defense")
+                print("Your current Defense also increased also to "+str(exp+Defense)+" Defense")
+          elif add==4 :
+                print("Your max Magic has increased to "+str( exp+...)+" magic")
+                print("Your current health also increased also to "+str(exp+Magic)+" magic")
+          else : # add==5
+                print("Your max Luck has increased to "+str( exp+...)+" luck")
+                print("Your current Luck also increased also to "+str(exp+Luck)+" luck")
+     else :
+          print("Attribute does not exist")
+
+
+
+
+def Obtain_Gold():
+	print("Lucky,you got "+str(gold)+" gold!")
+	print("Experience "+str(exp+10))
+
+     
+     
+     
+
---
+++ F09_switchcity.py
@@ -0,0 +1,23 @@
+from random import choice
+
+switch_city=[1,2]
+if choice(switch_city)==1:
+	print("Fast travelling...")
+	if city=="Solitude":
+		city=="Windhelm"
+		print("You have arrived at "+city)
+	else :  # city=="Windhelm"
+		city=="Solitude"
+		print("You have arrived at "+city)
+else : # choice(switch_)
+	print("Fast travelling...")
+	print("Oh no you can’t fast travel when enemies are nearby\nYou have met a tundra spider, will you fight or will you flee ( luck * 10% )? ")
+
+
+
+	
+
+         
+
+
+	
