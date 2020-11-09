07d5e79652f493da278b78dd96bd31407e40b194
erwinfattah <68496276+erwinfattah@users.noreply.github.com>
2020-11-09 20:38:06 +0700

    edit F09

--- F09_switchcity.py
+++ F09_switchcity.py
@@ -1,17 +1,19 @@
 import random
 
-switch_city=[1,2]
-if random.choice(switch_city)==1:
-	print("Fast travelling...")
-	if city=="Solitude":
-		city=="Windhelm"
-		print("You have arrived at "+city)
-	else :  # city=="Windhelm"
-		city=="Solitude"
-		print("You have arrived at "+city)
-else : # choice(switch_city)==2
-	print("Fast travelling...")
-	print("Oh no you can’t fast travel when enemies are nearby\nYou have met a tundra spider, will you fight or will you flee ( luck * 10% )? ")
+
+def Switch_City():
+	switch_city=[1,2]
+	if random.choice(switch_city)==1:
+		print("Fast travelling...")
+		if city=="Solitude":
+			city=="Windhelm"
+			print("You have arrived at "+city)
+		else :  # city=="Windhelm"
+			city=="Solitude"
+			print("You have arrived at "+city)
+	else : # choice(switch_city)==2
+		print("Fast travelling...")
+		print("Oh no you can’t fast travel when enemies are nearby\nYou have met a tundra spider, will you fight or will you flee ( luck * 10% )? ")
 
 
 
