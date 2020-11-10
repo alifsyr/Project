d600357f01a5ef01ed3abd6f32c43d0686a5674d
alifsyr <alifsyr@gmail.com>
2020-11-10 16:43:16 +0700

    Update F07_levelup.py

--- F07_levelup.py
+++ F07_levelup.py
@@ -1,10 +1,29 @@
-def Level_Up(currentUser,exp):
-     print("You got "+str(exp)+" EXP")
-     print("You leveled up!")
-     print("Choose what attribute to increase :")
-     print("1.Health\n2.Attack\n3.Defense\n4.Magic\n5.Luck")
+def Level_Up(currentUser):
+      print("You leveled up!")
+      print("Choose what attribute to increase :")
+      
+      attribute = [["1","Health","10"],["2","Attack","10"],["3","Defense","10"],["4","Magic","10"],["5","Luck","10"]]
+      for i in attribute:
+            print(i[0]+".",i[1]+" +"+i[2])
+      
+      result = input("add attribute: ")
+      for i in attribute:
+            if result == i[0]:
+                  if i[1] == 'Health':
+                        upgrade = int(currentUser[2]) + 10
+                        currentUser[2] = str(upgrade)
+                        HP = int(currentUser[3]) + 10
+                        currentUser[3] = str(HP)
+
+                        return currentUser
+
+                  elif i[0] == 'Attack':
+                  
+                  elif i[0] == ''
+                  
+      '''
      add=int(input("add attribute : "))
-     if add==1 or add==2 or add==3 or add=4 or add==5 :
+     if add==1 or add==2 or add==3 or add==4 or add==5 :
           if add==1 :
           	    currentUser[2]=int(currentUser[2])+10
                 currentUser[2]=str(currentUser[2])
@@ -36,6 +55,7 @@ def Level_Up(currentUser,exp):
      else :
           print("Attribute does not exist")
           return currentUser
+          '''
 
 
 
