58aa4448722d0af332f6a457d66552caf4ca0a34
alizayn10 <alizayn.murteza@gmail.com>
2020-11-12 11:28:39 +0700

    admin fix

--- F13_Administrator.py
+++ F13_Administrator.py
@@ -10,7 +10,7 @@ while True:
         database = int(input("database: ")) #awal lgsg muncul ini
         if database == 1:
             print("Showing data of all item:")
-            for i in item_data: #display data item
+            for i in item_arr: #display data item
                 if i[1] != "Nama" or i[2]!="Attack" or i[3]!="Magic" or i[4] != "Defense" or i[5] != "Luck" or i[6] != 'HP' or i[7] != "City" or i[8] != "Price":
                     print(i[1]+"|"+i[2]+" atk|"+i[4]+" def|"+i[5]+" luck|"+i[6]+" health|"+i[3]+" magic|"+i[7]+"|"+i[8]+" gold")
         elif database == 2: #display monster
@@ -26,7 +26,7 @@ while True:
                 database = int(input("database: "))
                 if database == 1:
                     print("Showing data of all item:")
-                    for i in item_data:
+                    for i in item_arr:
                         if i[1] != "Nama" or i[2]!="Attack" or i[3]!="Magic" or i[4] != "Defense" or i[5] != "Luck" or i[6] != 'HP' or i[7] != "City" or i[8] != "Price":
                             print(i[1]+"|"+i[2]+" atk"+"|"+i[4]+" def"+"|"+i[5]+" luck"+"|"+i[6]+" health"+"|"+i[3]+" magic"+"|"+i[7]+"|"+i[8]+" gold")
                 elif database == 2:
@@ -47,16 +47,16 @@ while True:
                 item_arr += [[len(item_arr),item_name,item_atk,item_magic,item_def,item_luck,item_health,item_region,item_gold]] #masukin ke array sementara (magic sm price ga ada di spesifikasi)
                 print("data successfully added to database")
             elif command == "delete":
-                for i in item_data: #display data item
+                for i in item_arr: #display data item
                     if i[1] != "Nama" or i[2]!="Attack" or i[3]!="Magic" or i[4] != "Defense" or i[5] != "Luck" or i[6] != 'HP' or i[7] != "City" or i[8] != "Price":
                         print(i[0]+". "+i[1]+"|"+i[2]+" atk|"+i[4]+" def|"+i[5]+" luck|"+i[6]+" health|"+i[3]+" magic|"+i[7]+"|"+i[8]+" gold")
                 delete = input("Which number: ")
-                for i in item_data:
+                for i in item_arr:
                     if i[1] != "Nama" or i[2]!="Attack" or i[3]!="Magic" or i[4] != "Defense" or i[5] != "Luck" or i[6] != 'HP' or i[7] != "City" or i[8] != "Price":
                         if i[0] != delete:
                             print(i[1]+"|"+i[2]+" atk|"+i[4]+" def|"+i[5]+" luck|"+i[6]+" health|"+i[3]+" magic|"+i[7]+"|"+i[8]+" gold") #baru di display doang blm dihapus di arraynya
                 new_item_arr = []
-                for i in item_data:
+                for i in item_arr:
                     if i[0] != delete:
                         new_item_arr +=[i]
                 item_arr = new_item_arr
