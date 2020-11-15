497ec1fd4b24fb9c8c18032479c2bbfc609c34f8
alifsyr <alifsyr@gmail.com>
2020-11-15 22:38:26 +0700

    Update F03_explore.py

--- F03_explore.py
+++ F03_explore.py
@@ -1,7 +1,7 @@
 import modules
 
 def explore(currentUser):
-    result = modules.randomrange(500)
+    result = modules.randomrange(501)
     if result >= (500 - int(currentUser[8])):
         gold = True
         return  gold
